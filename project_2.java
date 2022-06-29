a5f5f90498e9b744c510d9dacf6be049d60cd095
Olga Nazarova <mazon2010@yandex.ru>
2022-06-29 10:24:18 +0300

    added new task

--- project_2.java
+++ project_2.java
@@ -1,2 +1,18 @@
-public class project_2 {
+//Написать программу вычисления n-ого треугольного числа
+public class project_2{
+    public static void main(String args[]){
+        int n = 4;
+        towerOfHanoi(n, 'A', 'C', 'B');
+    }
+    public static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) {
+        if (n == 1) {
+            System.out.println(" Переместить диск с 1 стержня " + from_rod + " на стрежень " + to_rod);
+            return;
+        }
+
+        towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+
+        System.out.println(" Переместить диск " + n + " со стрежня " + from_rod + " на стержень " + to_rod);
+        towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+    }
 }
--- project_3.java
+++ project_3.java
@@ -1,2 +1,11 @@
-public class project_3 {
+public class project_3{
+    public static void main(String[] args){
+        int n = 6;
+        System.out.println(triangleFun(n));
+    }
+
+    public static int triangleFun(int n) {
+        if(n==1) return 1;
+        else return( n + triangleFun(n-1) );
+    }
 }
